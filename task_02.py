44899796b3e6354db445603238fa53ebd8be4a4a
William Cole <billy.cole@gmail.com>
2016-04-11 10:30:32 -0400

    Debugging

---
+++ task_02.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""CustomError"""
+
+class CustomError(Exception):
+    """Class CustomError, Subclass of Exception"""
+
+    def __init__(self, msg, cause):
+        """CustomError Constructor.
+        Arguments:
+            Exception: Self(for its constructor)
+            msg(string): Message
+            cause(string): Input
+        Returns:
+            A Custom Error
+        Examples:
+            >>> myerror = CustomError('Danger Will Robinson',
+            cause='Aliens Intruding')
+            >>> print myerror.cause
+            Aliens Intruding
+        """
+        msg = Exception.__init__(self, msg)
+        self.cause = cause
