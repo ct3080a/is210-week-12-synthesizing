34d8cebf2fa15b00682b4f4073fec6d989ac89a2
William Cole <billy.cole@gmail.com>
2016-04-11 10:28:09 -0400

    Custom Class

---
+++ task_01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Exception Classes"""
+
+
+class BaseError(Exception):
+    """BaseError Class Exception"""
+    pass
+
+class StringError(BaseError, TypeError):
+    """StringError Class"""
+    pass
+
+class NumberError(BaseError, TypeError):
+    """NumberError Class"""
+    pass
+
+class NonZeroError(NumberError):
+    """NonZeroError Class"""
+    pass
