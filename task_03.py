ab44d94e563c3e994fcae44f3cda5e0677387cd5
paisely65 <cunysps@gmail.com>
2016-04-15 18:15:38 -0400

    Task 3- spacing fixed.

--- task_03.py
+++ task_03.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 """Task 03 module"""
 
+
 def exception_test(arg1, arg2, arg3):
     """This function catches three types or errors."""
-
-
+    
     caught = False
     try:
         arg1[arg2].index(arg3)
