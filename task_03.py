4a3e4a6e6bf68035856bd522631bce622f98584f
paisely65 <cunysps@gmail.com>
2016-04-15 18:11:45 -0400

    Task3 is finished.

--- task_03.py
+++ task_03.py
@@ -3,6 +3,9 @@
 """Task 03 module"""
 
 def exception_test(arg1, arg2, arg3):
+    """This function catches three types or errors."""
+
+
     caught = False
     try:
         arg1[arg2].index(arg3)
@@ -10,4 +13,3 @@ def exception_test(arg1, arg2, arg3):
         caught = True
 
     return caught
-
