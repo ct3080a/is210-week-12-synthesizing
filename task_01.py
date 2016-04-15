98a503e3a5c7d8f105d2270dacd3a1a89f2a38f4
paisely65 <cunysps@gmail.com>
2016-04-15 16:23:33 -0400

    task1.

---
+++ task_01.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Starter data module"""
+
+
+class BaseError(Exception):
+    """This is the Parent Class."""
+
+    def init __self__()
+    
+    
+class StringError(BaseError, TypeError):
+    
+
+    
+class NumberError(BaseError`, ``TypeError):
+    """Sublass of BaseError` and `TypeError."""
+
+                  
+class NonZeroError(NumberError):
+    """Subclass of NumberError."""
+
+   
